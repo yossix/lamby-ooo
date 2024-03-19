78697ea97519d7498554ec6508423297862711d1
yossix <attiayossi@gmail.com>
2024-03-19 14:27:24 +0100

    remove multiprocess rack constants

--- lib/lamby/rack_http.rb
+++ lib/lamby/rack_http.rb
@@ -43,7 +43,7 @@ module Lamby
         ::Rack::RACK_INPUT => StringIO.new(body || ''),
         ::Rack::RACK_ERRORS => $stderr,
         #::Rack::RACK_MULTITHREAD => false,
-        ::Rack::RACK_MULTIPROCESS => false,
+        #::Rack::RACK_MULTIPROCESS => false,
         ::Rack::RACK_RUNONCE => false,
         LAMBDA_EVENT => event,
         LAMBDA_CONTEXT => context
